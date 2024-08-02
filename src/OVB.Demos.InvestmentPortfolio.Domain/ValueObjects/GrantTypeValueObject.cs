1be0b8f1a306ee0de85356818634f0163705799f
OtavioVB <otaviovb.developer@gmail.com>
2024-08-02 09:05:50 -0300

    feature: grant type value object and xunit validation tests

---
+++ src/OVB.Demos.InvestmentPortfolio.Domain/ValueObjects/GrantTypeValueObject.cs
@@ -0,0 +1,60 @@
+﻿using OVB.Demos.InvestmentPortfolio.Domain.Utils.MethodResultContext;
+using OVB.Demos.InvestmentPortfolio.Domain.Utils.NotificationContext;
+using OVB.Demos.InvestmentPortfolio.Domain.Utils.NotificationContext.Interfaces;
+using OVB.Demos.InvestmentPortfolio.Domain.ValueObjects.Exceptions;
+
+namespace OVB.Demos.InvestmentPortfolio.Domain.ValueObjects;
+
+public readonly struct GrantTypeValueObject
+{
+    public MethodResult<INotification> MethodResult { get; }
+    private string? GrantType { get; }
+
+    private GrantTypeValueObject(MethodResult<INotification> methodResult, string? grantType = null)
+    {
+        MethodResult = methodResult;
+        GrantType = grantType;
+    }
+
+    public readonly static string[] ALLOWED_GRANT_TYPES = { "password" };
+
+    private const string GRANT_TYPE_MUST_BE_VALID_NOTIFICATION_CODE = "GRANT_TYPE_MUST_BE_VALID";
+    private const string GRANT_TYPE_MUST_BE_VALID_NOTIFICATION_MESSAGE = "O valor de concessão da autorização deve ser um válido ('password').";
+
+    public static GrantTypeValueObject Factory(string grantType)
+    {
+        var isValidGrantType = false;
+
+        foreach (var allowedGrantType in ALLOWED_GRANT_TYPES)
+            if (allowedGrantType == grantType)
+            {
+                isValidGrantType = true;
+                break;
+            }
+
+        if (!isValidGrantType)
+            return new GrantTypeValueObject(
+                methodResult: MethodResult<INotification>.FactoryError(
+                    notifications: [Notification.FactoryFailure(
+                        code: GRANT_TYPE_MUST_BE_VALID_NOTIFICATION_CODE,
+                        message: GRANT_TYPE_MUST_BE_VALID_NOTIFICATION_MESSAGE)]));
+
+        return new GrantTypeValueObject(
+            methodResult: MethodResult<INotification>.FactorySuccess(),
+            grantType: grantType);
+    }
+
+    public string GetGrantType()
+    {
+        BusinessValueObjectException.ThrowExceptionMethodResultIsError(MethodResult);
+
+        return BusinessValueObjectException.ThrowExceptionIfTheObjectCannotBeNull(GrantType);
+    }
+
+    public static implicit operator GrantTypeValueObject(string obj)
+        => Factory(obj);
+    public static implicit operator string(GrantTypeValueObject obj)
+        => obj.GetGrantType();
+    public static implicit operator MethodResult<INotification>(GrantTypeValueObject obj)
+        => obj.MethodResult;
+}
---
+++ tsts/OVB.Demos.InvestmentPortfolio.UnitTests/Domain/ValueObjects/GrantTypeValueObjectValidationTests.cs
@@ -0,0 +1,62 @@
+﻿using OVB.Demos.InvestmentPortfolio.Domain.ValueObjects;
+using OVB.Demos.InvestmentPortfolio.Domain.ValueObjects.Exceptions;
+
+namespace OVB.Demos.InvestmentPortfolio.UnitTests.Domain.ValueObjects;
+
+public sealed class GrantTypeValueObjectValidationTests
+{
+    [Theory]
+    [InlineData("password")]
+    public void Grant_Type_Value_Object_Should_Be_Valid(string grantType)
+    {
+        // Arrange
+
+        // Act
+        var grantTypeValueObject = GrantTypeValueObject.Factory(
+            grantType: grantType);
+
+        // Assert
+        Assert.True(grantTypeValueObject.MethodResult.IsSuccess);
+        Assert.Empty(grantTypeValueObject.MethodResult.Notifications);
+        Assert.Equal(grantType, grantTypeValueObject.GetGrantType());
+        Assert.Equal(grantType, grantTypeValueObject);
+    }
+
+    [Theory]
+    [InlineData("client_credentials")]
+    [InlineData("authorization")]
+    public void Grant_Type_Value_Object_Should_Be_Not_Valid(string grantType)
+    {
+        // Arrange
+
+        // Act
+        var grantTypeValueObject = GrantTypeValueObject.Factory(
+            grantType: grantType);
+
+        // Assert
+        Assert.False(grantTypeValueObject.MethodResult.IsSuccess);
+        Assert.NotEmpty(grantTypeValueObject.MethodResult.Notifications);
+        Assert.Throws<BusinessValueObjectException>(() => grantTypeValueObject.GetGrantType());
+    }
+
+    [Theory]
+    [InlineData("client_credentials")]
+    [InlineData("authorization")]
+    public void Grant_Type_Value_Object_Should_Be_Send_Notification_Error_As_Expected_When_Grant_Type_Is_Not_Valid(string grantType)
+    {
+        // Arrange
+        const string EXPECTED_CODE = "GRANT_TYPE_MUST_BE_VALID";
+        const string EXPECTED_MESSAGE = "O valor de concessão da autorização deve ser um válido ('password').";
+        const string EXPECTED_TYPE = "Failure";
+
+        // Act
+        var grantTypeValueObject = GrantTypeValueObject.Factory(
+            grantType: grantType);
+
+        // Assert
+        Assert.Single(grantTypeValueObject.MethodResult.Notifications);
+        Assert.Equal(EXPECTED_CODE, grantTypeValueObject.MethodResult.Notifications[0].Code);
+        Assert.Equal(EXPECTED_MESSAGE, grantTypeValueObject.MethodResult.Notifications[0].Message);
+        Assert.Equal(EXPECTED_TYPE, grantTypeValueObject.MethodResult.Notifications[0].Type);
+    }
+}
