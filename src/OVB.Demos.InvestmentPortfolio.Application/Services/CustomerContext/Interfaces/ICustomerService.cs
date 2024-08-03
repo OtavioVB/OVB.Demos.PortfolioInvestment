dd2bf5d018b6542204af449c3f1a4d41b5e75c1e
OtavioVB <otaviovb.developer@gmail.com>
2024-08-03 13:52:49 -0300

    feature: oauth customer authentication service

---
+++ src/OVB.Demos.InvestmentPortfolio.Application/Services/CustomerContext/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using OVB.Demos.InvestmentPortfolio.Application.Services.CustomerContext.Inputs;
+using OVB.Demos.InvestmentPortfolio.Application.Services.CustomerContext.Outputs;
+using OVB.Demos.InvestmentPortfolio.Domain.Utils.MethodResultContext;
+using OVB.Demos.InvestmentPortfolio.Domain.Utils.NotificationContext.Interfaces;
+
+namespace OVB.Demos.InvestmentPortfolio.Application.Services.CustomerContext.Interfaces;
+
+public interface ICustomerService
+{
+    public Task<MethodResult<INotification, OAuthCustomerAuthenticationServiceOutput>> OAuthCustomerAuthenticationServiceAsync(
+        OAuthCustomerAuthenticationServiceInput input,
+        CancellationToken cancellationToken);
+}
